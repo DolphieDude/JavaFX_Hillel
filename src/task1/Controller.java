e137f3b66f45264a9f1c8ed51853fb7fc7565c4e
DolphieDude <andreycherevat@gmail.com>
2020-01-28 17:22:47 +0200

    rename package

--- src/task1/Controller.java
+++ src/task1/Controller.java
@@ -1,9 +1,7 @@
-package sample;
+package task1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
